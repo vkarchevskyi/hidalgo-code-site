2913d1232425d52fe6fdad2cc0a6e9063fa5b4d4
vkarchevskyi <vkarchevskyi10@gmail.com>
2025-04-06 19:00:50 +0300

    feat: create Game and Award type

--- src/lib/utils/types.ts
+++ src/lib/utils/types.ts
@@ -1,40 +1,67 @@
 export type NoUndefinedField<T> = { [P in keyof T]-?: NoUndefinedField<NonNullable<T[P]>> };
 
 export type SparkleType = {
-  id: string,
-  createdAt: number,
-  color: string,
-  size: number,
-  style: any
-}
+	id: string;
+	createdAt: number;
+	color: string;
+	size: number;
+	style: any;
+};
 
 export type TagType = {
-  label: string,
-  color?: 'primary' | 'secondary'
-}
+	label: string;
+	color?: 'primary' | 'secondary';
+};
 
-export type SocialLink = {
-  
-}
+export type SocialLink = {};
 
 export type Feature = {
-  name: string,
-  description: string,
-  image: string,
-  tags: TagType[]
-}
+	name: string;
+	description: string;
+	image: string;
+	tags: TagType[];
+};
 
 export type BlogPost = {
-  tags: string[],
-  keywords: string[],
-  hidden: boolean,
-  slug: string,
-  title: string,
-  date: string,
-  updated: string,
-  excerpt: string,
-  html: string | undefined,
-  readingTime: string,
-  relatedPosts: BlogPost[],
-  coverImage: string | undefined
+	tags: string[];
+	keywords: string[];
+	hidden: boolean;
+	slug: string;
+	title: string;
+	date: string;
+	updated: string;
+	excerpt: string;
+	html: string | undefined;
+	readingTime: string;
+	relatedPosts: BlogPost[];
+	coverImage: string | undefined;
+};
+
+export interface Game {
+	slug: string;
+	title: string;
+	shortDescription: string;
+	description: string;
+	coverImage: string;
+	gallery?: string[];
+	trailer?: string;
+	releaseDate?: Date;
+	features?: { title: string; description: string; icon?: string }[];
+	systemRequirements?: {
+		minimal: { [key: string]: string };
+		recommended?: { [key: string]: string };
+	};
+	platforms?: string[];
+	price?: string;
+	storeLinks?: { platform: string; url: string }[];
+	tags?: string[];
+	awards?: Award[];
 }
+
+export type Award = {
+	name: string;
+	organization: string;
+	year: number;
+	image?: string;
+	description?: string;
+};
